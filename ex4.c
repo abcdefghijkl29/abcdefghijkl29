3a619d5b2a8998da37e82cf47470a5d3d2e4c4e1
abcdefghijkl29 <100131518+abcdefghijkl29@users.noreply.github.com>
2022-03-07 20:04:34 +0800

    Create ex4.c

---
+++ ex4.c
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myAge = 14;
+  int height = 167;
+  int mybrothersandmysisters = 0;
+  cout << "I am " << myAge << "14";
+  cout << "I am " << height<< " 167";
+  cout << "I am " << mybrothersandmysisters<< "0";
+  return 0;
+}
