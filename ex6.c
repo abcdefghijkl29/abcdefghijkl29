9ca1cc52c024ff60d1b294768de86c60016c329f
abcdefghijkl29 <100131518+abcdefghijkl29@users.noreply.github.com>
2022-03-14 20:28:49 +0800

    Create ex6.c

---
+++ ex6.c
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+******************************************************************************/
+#include <stdio.h>
+x + y
+x - y
+x * y
+x / y
+x % y
+++x
+x++
+--x
+x--
+
+x = 16 
+x >> 1
+x >> 2
+x << 1
